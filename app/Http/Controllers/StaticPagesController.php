8e0fc37a492a3d6963aa53804cd9e7835a92d3b9
vagrant <vagrant@homestead>
2018-01-08 03:54:51 +0000

    hello

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home()
+    {
+        return '首页';
+    }
+
+    public function about()
+    {
+        return '关于页';
+    }
+
+    public function help()
+    {
+        return '帮助页';
+    }
+}
